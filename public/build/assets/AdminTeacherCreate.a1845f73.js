7d92ff330c107975054cfc32c50534dc6dc7f8ba
Redtro2k <llacuna.darwin24@gmail.com>
2022-11-16 01:07:40 +0800

    2nd deploy

---
+++ public/build/assets/AdminTeacherCreate.a1845f73.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";const r={};function c(n,t){return null}const a=e(r,[["render",c]]);export{a as default};
---
+++ public/build/assets/GlobalSchoolIndex.2d6c4055.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./_plugin-vue_export-helper.cdc0426e.js";const e={};function n(o,r){return null}const _=c(e,[["render",n]]);export{_ as default};
