571d48a85e27f628ba483bd8070f8195bc8eed2a
Redtro2k <llacuna.darwin24@gmail.com>
2022-11-29 01:30:35 +0800

    add birthdate on user

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,8 @@ class User extends Authenticatable
         'middlename',
         'username',
         'password',
-        'gender'
+        'gender',
+        'birthdate'
     ];
     protected $hidden = [
         'password',
